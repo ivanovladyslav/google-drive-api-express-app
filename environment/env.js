0717d1ef3764a88273f99777c1ce7dd3a704716c
ivanovladyslav <ivanovladyslav@gmail.com>
2019-11-02 20:40:05 +0700

    Env fix for heroku

--- environment/env.js
+++ environment/env.js
@@ -1,4 +1,4 @@
 const localhost = "http://127.0.0.1:4000"
-const heroku = "https://mysterious-forest-81855.herokuapp.com"
+const heroku = "https://sci-assist-backend.herokuapp.com"
 
-module.exports = URL = localhost;
+module.exports = URL = heroku;
